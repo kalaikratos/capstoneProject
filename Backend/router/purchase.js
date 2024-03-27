686f20b93fcb1f02fee100d10da9e688a6514349
kalaikratos <kalaiarasan2796@gmail.com>
2024-03-27 07:54:44 +0100

    Initial commit

--- Backend/router/purchase.js
+++ Backend/router/purchase.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const purchase = require("../controller/Purchase");
+const purchase = require("../controller/purchase");
 
 // Add Purchase
 app.post("/add", purchase.addPurchase);
