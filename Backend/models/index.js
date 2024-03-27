8429dabbd93036d6044c73bafced500627204e6f
kalaikratos <kalaiarasan2796@gmail.com>
2024-03-27 07:49:58 +0100

    Initial commit

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const uri = "mongodb+srv://kalai:kalai@cluster0.fx3auzt.mongodb.net/";
+const uri = "mongodb+srv://kalai:kalai@cluster0.fx3auzt.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
 
 function main() {
     mongoose.connect(uri).then(() => {
